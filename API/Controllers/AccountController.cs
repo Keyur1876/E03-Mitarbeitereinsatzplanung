ee1146d0c362083d0deed579bfe8eebc38b18109
Keyur1876 <139059412+Keyur1876@users.noreply.github.com>
2023-11-12 13:38:42 +0100

    Change

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
 using API.Data;
 using API.DTOs;
@@ -11,29 +10,44 @@ namespace API.Controllers
 {
     public class AccountController : BaseApiController
     {
+        private readonly ITokenService _tokenService;
         private readonly DataContext _context;
 
-        public AccountController(DataContext context) : base(context)
+        // Constructor to initialize the AccountController with DataContext and ITokenService
+        public AccountController(DataContext context, ITokenService tokenService) : base(context)
         {
+            _tokenService = tokenService;
             _context = context;
         }
 
+        // Data transfer object (DTO) for user registration
         public class RegisterDto
         {
             public string Username { get; set; }
             public string Password { get; set; }
         }
 
-        [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        // Data transfer object (DTO) for user login
+        public class LoginDto
         {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+
+        // Endpoint for user registration
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            // Check if the username is already taken
             if (await UserExists(registerDto.Username))
             {
                 return BadRequest("Username is taken");
             }
 
+            // Use HMACSHA512 for password hashing
             using var hmac = new HMACSHA512();
 
+            // Create a new AppUser with the provided username and hashed password
             var user = new AppUser
             {
                 UserName = registerDto.Username,
@@ -41,32 +55,49 @@ namespace API.Controllers
                 PasswordSalt = hmac.Key
             };
 
+            // Add the user to the database and save changes
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            return user;
+            // Return a UserDto containing the username and a JWT token
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
+        // Endpoint for user login
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>>Login(LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
+            // Find the user with the provided username in the database
             var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
+            // If the user is not found, return Unauthorized
             if (user == null) return Unauthorized("Invalid Username");
 
+            // Use the stored password salt to create an HMACSHA512 instance
             using var hmac = new HMACSHA512(user.PasswordSalt);
 
+            // Compute the hash of the provided password
             var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
-            for(int i = 0; i< computedHash.Length; i++ )
+            // Compare the computed hash with the stored password hash
+            for (int i = 0; i < computedHash.Length; i++)
             {
-                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
             }
 
-            return user;
-           
+            // If the password is valid, return a UserDto with the username and a JWT token
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
+        // Private method to check if a user with a given username already exists
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
