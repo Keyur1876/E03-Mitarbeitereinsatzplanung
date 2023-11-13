54c14590b832ad4edf97622543adbd852a36b17f
Keyur1876 <139059412+Keyur1876@users.noreply.github.com>
2023-11-13 22:25:37 +0100

    JWT-Token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -13,53 +15,44 @@ namespace API.Controllers
         private readonly ITokenService _tokenService;
         private readonly DataContext _context;
 
-        // Constructor to initialize the AccountController with DataContext and ITokenService
         public AccountController(DataContext context, ITokenService tokenService) : base(context)
         {
             _tokenService = tokenService;
             _context = context;
         }
 
-        // Data transfer object (DTO) for user registration
         public class RegisterDto
         {
             public string Username { get; set; }
             public string Password { get; set; }
         }
 
-        // Data transfer object (DTO) for user login
         public class LoginDto
         {
             public string Username { get; set; }
             public string Password { get; set; }
         }
 
-        // Endpoint for user registration
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
-            // Check if the username is already taken
             if (await UserExists(registerDto.Username))
             {
                 return BadRequest("Username is taken");
             }
 
-            // Use HMACSHA512 for password hashing
             using var hmac = new HMACSHA512();
 
-            // Create a new AppUser with the provided username and hashed password
             var user = new AppUser
             {
-                UserName = registerDto.Username,
+                UserName = registerDto.Username.ToLower(),
                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
 
-            // Add the user to the database and save changes
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            // Return a UserDto containing the username and a JWT token
             return new UserDto
             {
                 Username = user.UserName,
@@ -67,29 +60,27 @@ namespace API.Controllers
             };
         }
 
-        // Endpoint for user login
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            // Find the user with the provided username in the database
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName.ToLower() == loginDto.Username.ToLower());
 
-            // If the user is not found, return Unauthorized
-            if (user == null) return Unauthorized("Invalid Username");
-
-            // Use the stored password salt to create an HMACSHA512 instance
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-
-            // Compute the hash of the provided password
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            // Compare the computed hash with the stored password hash
-            for (int i = 0; i < computedHash.Length; i++)
+            if (user == null)
             {
-                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+                return Unauthorized("Invalid Username");
             }
 
-            // If the password is valid, return a UserDto with the username and a JWT token
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                {
+                    return Unauthorized("Invalid Password");
+                }
+            }
+
             return new UserDto
             {
                 Username = user.UserName,
@@ -97,7 +88,6 @@ namespace API.Controllers
             };
         }
 
-        // Private method to check if a user with a given username already exists
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
