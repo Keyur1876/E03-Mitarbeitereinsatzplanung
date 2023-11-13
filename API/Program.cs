ae83a103b3ddc384a78e168fa56566bd8ae04281
Keyur1876 <139059412+Keyur1876@users.noreply.github.com>
2023-11-13 12:13:15 +0100

    change

--- API/Program.cs
+++ API/Program.cs
@@ -7,9 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddDbContext<DataContext>(opt => {  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));});
 
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
