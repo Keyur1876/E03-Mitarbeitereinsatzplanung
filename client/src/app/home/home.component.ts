e4af1778964c644d617ebe05d24d4a46ec48b348
PureHorizon <wagner.luca@hotmail.com>
2023-12-15 17:26:50 +0100

    Erstellen eines Login Fensters

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 // src/app/home/home.component.ts
 
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AccountService } from '../services/account.service';
 
 @Component({
@@ -13,8 +13,13 @@ export class HomeComponent implements OnInit {
   registerMode = false;
   users: any;
   isLoggedIn = false;
+  activeMenuItem: string = '';
+  showPopup: boolean = false;
 
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  @ViewChild('passwordInput') passwordInput!: ElementRef;
+  @ViewChild('togglePassword') togglePassword!: ElementRef;
+
+  constructor(private http: HttpClient, private accountService: AccountService) { }
 
   ngOnInit(): void {
     this.getUsers();
@@ -34,32 +39,40 @@ export class HomeComponent implements OnInit {
       complete: () => console.log('Request has completed'),
     });
   }
+
   cancelRegisterMode(event: boolean) {
     this.registerMode = event;
   }
-
-  activeMenuItem: string = '';
 
   setActive(menuItem: string): void {
     this.activeMenuItem = menuItem;
   }
 
-  showPopup: boolean = false;
-
-  // Funktion zum Öffnen des Popups
   openPopup(): void {
     this.showPopup = true;
   }
 
-  // Funktion zum Schließen des Popups
   closePopup(): void {
     this.showPopup = false;
   }
 
-  // Funktion zum Schließen des Popups bei Klick außerhalb
   onPopupClick(event: Event): void {
     if ((event.target as HTMLElement).id === 'news-form-popup') {
       this.closePopup();
     }
+  }
+
+  togglePasswordVisibility(): void {
+    const input = this.passwordInput.nativeElement;
+    const toggle = this.togglePassword.nativeElement;
+    const type = input.type === 'password' ? 'text' : 'password';
+    input.type = type;
+    toggle.textContent = type === 'password' ? 'visibility_off' : 'visibility';
+  }
+
+  onLoginFormSubmit(event: Event): void {
+    event.preventDefault();
+    // Hier würde Ihre Logik zum Überprüfen der Anmeldeinformationen stehen
+    // und ggf. die Authentifizierung über den AccountService ausführen.
   }
 }
